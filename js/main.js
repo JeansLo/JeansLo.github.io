0e72e8480bfc475ffaa734fb2d5396300d6a6630
JeansLo <jeanslo1912@gmail.com>
2025-01-27 13:43:08 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 $(()=>
 {
     $( "body > span" ).hide( 0 ) ; 
-    $( "#header > a:not(:first-child)" ).hide( 0 ) ; 
-    $( "#header > svg" ).on( "click" , ()=>
-        {
-            console.log( "hi" ) ; 
-            $( "#header #dropDown" ).show() ; 
-        } ) ; 
 });
