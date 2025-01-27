c8f542a26249821749b4e57b770588f20ca2bcc5
JeansLo <jeanslo1912@gmail.com>
2025-01-27 13:43:21 +0800

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,4 +1,9 @@
 $(()=>
 {
-    
+    $( "#header > a:not(:first-child)" ).hide( 0 ) ; 
+    $( "#header > svg" ).on( "click" , ()=>
+        {
+            console.log( "hi" ) ; 
+            $( "#header #dropDown" ).toggle() ; 
+        } ) ; 
 });
